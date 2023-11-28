abef65992bcbe867bc8d1717b7ef95781c1a18ac
Fahad.Hassan <fahadfuuast2@gmail.com>
2023-11-28 22:54:46 +0500

    JS_Operators

---
+++ app.js
@@ -0,0 +1,87 @@
+// console.log("Operators in JS");
+
+// Arthematic Operators
+
+// let a = 10;
+// let b = 4;
+
+// console.log("a + b =", a+b);
+// console.log("a - b =", a-b);
+// console.log("a * b =", a*b);
+// console.log("a / b =", a/b);
+// console.log("a % b =", a%b);
+// console.log("a++ =", a++);
+// console.log("a =", a);
+// console.log("++a =", ++a);
+// console.log("a =", a);
+// console.log("a-- =", a--);
+// console.log("a =", a);
+// console.log("--a =", --a);
+// console.log("a =", a);
+
+// console.log("Assignment Operators");
+
+// let assignment = 1;
+// console.log("Addition");
+// same as assignment = assignment + 5;
+// assignment += 5;  
+// console.log("assignment =", assignment);
+
+// console.log("Subtraction");
+// same as assignment = assignment - 5;
+// assignment -= 5;
+// console.log("assignment =",assignment);
+
+// console.log("Multiplication");
+// same as assignment = assignment * 5;
+// assignment *= 5;
+// console.log("assignment =", assignment)
+
+// console.log("Division");
+// same as assignment = assignment / 5;
+// assignment /= 5;
+// console.log("assignment =", assignment);
+
+// console.log("Reminder");
+// same as assignment = assignment % 5;
+// assignment %= 5;
+// console.log("assignment =", assignment);
+
+// console.log("Comparison Operators");
+
+// let comp1 = 6;
+// let comp2 = 7;
+// let comp3 = "6";
+
+// console.log("comp1 == comp2: ", comp1 == comp2);
+// console.log("comp1 != comp2: ", comp1 != comp2);
+// console.log("comp1 == comp3: ", comp1 == comp3);
+// console.log("comp1 != comp3: ", comp1 != comp3);
+
+// console.log("comp1 === comp2: ", comp1 === comp2);
+// console.log("comp1 !== comp2: ", comp1 !== comp2);
+// console.log("comp1 == comp3: ", comp1 === comp3);
+// console.log("comp1 != comp3: ", comp1 !== comp3);
+
+// console.log("comp1 > comp3: ", comp1 > comp2);
+// console.log("comp1 >= comp3: ", comp1 >= comp2);
+// console.log("comp1 > comp3: ", comp1 > comp3);
+// console.log("comp1 >= comp3: ", comp1 >= comp3);
+
+// console.log("comp1 < comp3: ", comp1 < comp2);
+// console.log("comp1 <= comp3: ", comp1 <= comp2);
+// console.log("comp1 < comp3: ", comp1 < comp3);
+// console.log("comp1 <= comp3: ", comp1 <= comp3);
+
+// console.log("Logical Operators");
+
+// let x = 5;
+// let y = 6;
+
+// console.log(x > y && y == 6);
+
+// console.log(x < y && y == 5 && x == 5);
+
+// console.log(x > y || y == 6);
+
+// console.log(x > y || y == 5 || x == 5);
